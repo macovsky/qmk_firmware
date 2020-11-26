56fe9d7e393023847174278b1d99091d75bc89b3
Max Savchenko <robotector@gmail.com>
2020-11-25 18:14:45 -0800

    use ctls to switch en/ru

--- keyboards/v60_type_r/keymaps/macovsky/keymap.c
+++ keyboards/v60_type_r/keymaps/macovsky/keymap.c
@@ -1,9 +1,12 @@
 #include QMK_KEYBOARD_H
 
-#define FN       MO(1)
-#define FN_CAPS  LT(1, KC_CAPS)
-#define SFT_CAPS RSFT(KC_CAPS)
-#define COMPOSE  KC_RALT
+#define FN        MO(1)
+#define FN_CAPS   LT(1, KC_CAPS)
+#define COMPOSE   KC_RALT
+
+/* switch locale */
+#define CTL_CAPS  LCTL_T(KC_CAPS)
+#define SFT_CAPS  RSFT(KC_CAPS)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   /* Keymap 0: Default Layer (Qwerty)
@@ -24,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 	       KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS, \
 	       FN_CAPS, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,  \
 	       KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,          KC_RSFT,   \
-	       KC_LCTL, KC_LGUI, KC_LALT,          KC_SPC,                                      FN,      KC_RGUI, COMPOSE, SFT_CAPS),
+	       CTL_CAPS,KC_LGUI, KC_LALT,          KC_SPC,                                      FN,      KC_RGUI, COMPOSE, SFT_CAPS),
 
   /* Keymap 1: FN Layer
    * ,-----------------------------------------------------------.
