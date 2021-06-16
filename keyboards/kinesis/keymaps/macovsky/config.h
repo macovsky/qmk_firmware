0fe0527ec359714fd3eaaecd775fd3e6e39c1614
Max Savchenko <atoms@mindscan.msk.ru>
2021-06-16 16:13:08 -0700

    compose as rctl, pg nav, tapping term

--- keyboards/kinesis/keymaps/macovsky/config.h
+++ keyboards/kinesis/keymaps/macovsky/config.h
@@ -5,8 +5,7 @@
 
 // https://beta.docs.qmk.fm/using-qmk/software-features/tap_hold#ignore-mod-tap-interrupt
 #define IGNORE_MOD_TAP_INTERRUPT
-// #define PERMISSIVE_HOLD
-#define TAPPING_TERM 300
+#define TAPPING_TERM 220
 
 // https://beta.docs.qmk.fm/using-qmk/software-features/one_shot_keys
 // #define ONESHOT_TAP_TOGGLE 2  /* Tapping this number of times holds the key until tapped once again. */
