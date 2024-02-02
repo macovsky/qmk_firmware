3adf99170a1215aacc6ac550d6818f407a526e92
Max Savchenko <atoms@mindscan.msk.ru>
2024-02-02 14:35:49 +0100

    fix compose

--- keyboards/xbows/nature/keymaps/macovsky-linux/keymap.c
+++ keyboards/xbows/nature/keymaps/macovsky-linux/keymap.c
@@ -6,9 +6,12 @@
 #define SYS 3
 #define RGB 4
 
+#define COMPOSE  KC_RCTL
+
 #define SYS_ESC LT(SYS, KC_ESC)
 #define RGB_RBR LT(RGB, KC_RBRC)
 #define SFT_CAPS SFT_T(KC_CAPS)
+#define RUS S(KC_CAPS)
 
 #define SFT_A SFT_T(KC_A)
 #define CTRL_S CTL_T(KC_S)
@@ -38,8 +41,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,    KC_F6,        KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,     KC_F12,   _______, KC_SLEP,
     KC_EQL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,       KC_MINS,  _______, DF(GAME),
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,       KC_BSLS,  _______, _______, _______,
-    SFT_CAPS,SFT_A,   CTRL_S,  ALT_D,   GUI_F,   KC_G,     KC_BSPC,      KC_H,    GUI_J,   ALT_K,   CTRL_L,  SFT_SCLN,   KC_QUOT,  _______, _______,
-    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     _______,      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,    _______,           _______,
+    SFT_CAPS,SFT_A,   CTRL_S,  ALT_D,   GUI_F,   KC_G,     KC_BSPC,      KC_H,    GUI_J,   ALT_K,   CTRL_L,  SFT_SCLN,   KC_QUOT,  RUS,     _______,
+    KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     _______,      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,    COMPOSE,           _______,
     TMUX,    KC_GRV,  _______,          MO(NAVI),      SYS_ESC, KC_ENT,  KC_SPC,           _______,          KC_LBRC,    RGB_RBR,  _______, _______, _______),
   [GAME] = LAYOUT(
     KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,    KC_F6,        KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,     KC_F12,   _______, KC_SLEP,
