0e39c9cdab6f9ef810b4737b747030f90b64bbd7
Max Savchenko <atoms@mindscan.msk.ru>
2024-04-12 11:53:51 +0200

    add lisp navigation, sleep in navi layer only

--- keyboards/xbows/nature/keymaps/macovsky-macos/keymap.c
+++ keyboards/xbows/nature/keymaps/macovsky-macos/keymap.c
@@ -44,21 +44,21 @@ enum custom_keycodes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [BASE] = LAYOUT(
-    KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,    KC_F6,        KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,     KC_F12,   _______, KC_SLEP,
+    KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,    KC_F6,        KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,     KC_F12,   _______, _______,
     KC_EQL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,       KC_MINS,  _______, DF(GAME),
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,       KC_BSLS,  _______, _______, _______,
     SFT_CAPS,SFT_A,   CTRL_S,  ALT_D,   GUI_F,   KC_G,     KC_BSPC,      KC_H,    GUI_J,   ALT_K,   CTRL_L,  SFT_SCLN,   KC_QUOT,  _______, _______,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     _______,      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,    _______,           _______,
     TMUX,    KC_GRV,  _______,          MO(NAVI),      SYS_ESC, KC_ENT,  KC_SPC,           _______,          KC_LBRC,    RGB_RBR,  _______, _______, _______),
   [GAME] = LAYOUT(
-    KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,    KC_F6,        KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,     KC_F12,   _______, KC_SLEP,
+    KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,    KC_F6,        KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,     KC_F12,   _______, _______,
     KC_EQL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,       KC_MINS,  DF(BASE),_______,
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,       KC_BSLS,  _______, _______, _______,
     SFT_CAPS,KC_A,    KC_S,    KC_D,    KC_F,    KC_G,     KC_BSPC,      KC_H,    GUI_J,   ALT_K,   CTRL_L,  SFT_SCLN,   KC_QUOT,  _______, _______,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     _______,      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,    _______,           _______,
     KC_GRV,  KC_GRV,  _______,          MO(NAVI),      SYS_ESC, KC_ENT,  KC_SPC,           _______,          KC_LBRC,    RGB_RBR,  _______, _______, _______),
   [NAVI] = LAYOUT(
-    _______, _______, _______, _______, _______, _______,   _______,     _______, _______, _______, KC_MUTE, KC_VOLD,    KC_VOLU,  _______, _______,
+    _______, _______, _______, _______, _______, _______,   _______,     _______, _______, _______, KC_MUTE, KC_VOLD,    KC_VOLU,  _______, KC_SLEP,
     _______, _______, _______, _______, _______, _______,                _______, _______, _______, _______, _______,    _______,  _______, _______,
     _______, QUIT,    _______, EMACS,   RELOAD,  TERM,                   KC_GRV,  KC_LBRC, KC_RBRC, _______, _______,    _______,  _______, _______, _______,
     _______, _______, _______, _______, _______, TELE,      KC_DEL,      KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,_______,    _______,  _______, _______,
