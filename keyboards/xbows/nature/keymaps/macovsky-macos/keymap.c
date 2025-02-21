0ef055daf16ddbce118b60d3e083d4d413669555
Max Savchenko <atoms@mindscan.msk.ru>
2025-02-21 08:58:12 +0100

    switch ru/en directly

--- keyboards/xbows/nature/keymaps/macovsky-macos/keymap.c
+++ keyboards/xbows/nature/keymaps/macovsky-macos/keymap.c
@@ -8,7 +8,7 @@
 
 #define SYS_ESC LT(SYS, KC_ESC)
 #define RGB_RBR LT(RGB, KC_RBRC)
-#define SFT_CAPS SFT_T(KC_CAPS)
+#define SFT_ENG SFT_T(KC_F17)
 
 /* mods */
 #define SFT_A SFT_T(KC_A)
@@ -21,7 +21,6 @@
 #define CTRL_L CTL_T(KC_L)
 #define SFT_SCLN SFT_T(KC_SCLN)
 
-#define QUIT G(KC_Q)
 #define BROWSER G(A(KC_B))
 #define RELOAD G(KC_R)
 #define TABL S(G(KC_LBRC))
@@ -31,11 +30,9 @@
 #define EMACS G(A(KC_E))
 #define TELE G(A(KC_G))
 #define ZOOM G(A(KC_Z))
-
-// TODO
-#define DISC G(A(KC_V))
-#define DISCJ S(G(A(KC_J)))
-#define DISCK S(G(A(KC_K)))
+#define DISC G(A(KC_D))
+// #define DISCJ S(G(A(KC_J)))
+// #define DISCK S(G(A(KC_K)))
 
 enum custom_keycodes {
   QWERTY = SAFE_RANGE,
@@ -47,29 +44,29 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,    KC_F6,        KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,     KC_F12,   _______, _______,
     KC_EQL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,       KC_MINS,  _______, DF(GAME),
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,       KC_BSLS,  _______, _______, _______,
-    SFT_CAPS,SFT_A,   CTRL_S,  ALT_D,   GUI_F,   KC_G,     KC_BSPC,      KC_H,    GUI_J,   ALT_K,   CTRL_L,  SFT_SCLN,   KC_QUOT,  _______, _______,
+    SFT_ENG, SFT_A,   CTRL_S,  ALT_D,   GUI_F,   KC_G,     KC_BSPC,      KC_H,    GUI_J,   ALT_K,   CTRL_L,  SFT_SCLN,   KC_QUOT,  KC_F18,  _______,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     _______,      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,    _______,           _______,
     TMUX,    KC_GRV,  _______,          MO(NAVI),      SYS_ESC, KC_ENT,  KC_SPC,           _______,          KC_LBRC,    RGB_RBR,  _______, _______, _______),
   [GAME] = LAYOUT(
     KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,    KC_F6,        KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,     KC_F12,   _______, _______,
     KC_EQL,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                   KC_6,    KC_7,    KC_8,    KC_9,    KC_0,       KC_MINS,  DF(BASE),_______,
     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                   KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,       KC_BSLS,  _______, _______, _______,
-    SFT_CAPS,KC_A,    KC_S,    KC_D,    KC_F,    KC_G,     KC_BSPC,      KC_H,    GUI_J,   ALT_K,   CTRL_L,  SFT_SCLN,   KC_QUOT,  _______, _______,
+    SFT_ENG, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,     KC_BSPC,      KC_H,    GUI_J,   ALT_K,   CTRL_L,  SFT_SCLN,   KC_QUOT,  _______, _______,
     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,     _______,      KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,    _______,           _______,
     KC_GRV,  KC_GRV,  _______,          MO(NAVI),      SYS_ESC, KC_ENT,  KC_SPC,           _______,          KC_LBRC,    RGB_RBR,  _______, _______, _______),
   [NAVI] = LAYOUT(
     _______, _______, _______, _______, _______, _______,   _______,     _______, _______, _______, KC_MUTE, KC_VOLD,    KC_VOLU,  _______, KC_SLEP,
     _______, _______, _______, _______, _______, _______,                _______, _______, _______, _______, _______,    _______,  _______, _______,
-    _______, QUIT,    _______, EMACS,   RELOAD,  TERM,                   KC_GRV,  KC_LBRC, KC_RBRC, _______, _______,    _______,  _______, _______, _______,
+    _______, _______, _______, EMACS,   RELOAD,  TERM,                   KC_GRV,  KC_LBRC, KC_RBRC, _______, _______,    _______,  _______, _______, _______,
     _______, _______, _______, _______, _______, TELE,      KC_DEL,      KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT,_______,    _______,  _______, _______,
-    _______, ZOOM,    DISC,    _______, _______, BROWSER,   _______,     _______, KC_PGDN, KC_PGUP, _______, _______,    _______,           _______,
+    _______, ZOOM,    _______, _______, DISC,    BROWSER,   _______,     _______, KC_PGDN, KC_PGUP, _______, _______,    _______,           _______,
     _______, _______, _______,          _______,       EMACS,   TERM,    BROWSER,          _______,          _______,    _______,  _______, _______, _______),
   [SYS] = LAYOUT(
     QK_BOOT, _______, _______, _______, _______, _______,   _______,     _______, _______, _______, KC_MUTE, KC_VOLD,    KC_VOLU,  _______, _______,
     _______, _______, _______, _______, _______, _______,                _______, _______, _______, _______, _______,    _______,  _______, _______,
     _______, _______, _______, _______, _______, _______,                _______, TABL,    TABR,    _______, _______,    _______,  _______, _______, _______,
     _______, _______, _______, _______, _______, _______,   _______,     KC_MS_L, KC_MS_D, KC_MS_U, KC_MS_R, _______,    _______,  _______, _______,
-    _______, _______, _______, _______, _______, _______,   _______,     _______, KC_WH_D, KC_WH_U, _______, _______,    _______,           _______,
+    _______, _______, _______, _______, _______, _______,   _______,     _______, KC_WH_U, KC_WH_D, _______, _______,    _______,           _______,
     _______, _______, _______,          _______,       _______, KC_BTN1, KC_BTN2,          _______,          _______,    _______,  _______, _______, _______),
   [RGB] = LAYOUT(
     _______, _______, _______, _______, _______, _______,   _______,     _______, _______, _______, _______, _______,    _______,  _______, _______,
